213d511598dc5ab7447fe81427e3c5bc0b21a4ca
Q <UwU>
2022-04-04 02:11:36 +0100

    pretty sure the stupid aspect container was a mistake, GUI scaling correctly now. just locking res at 1280 x 650.

--- proj_dir/hot_point_test_bed.gd
+++ proj_dir/hot_point_test_bed.gd
@@ -1,10 +1,10 @@
 extends Area2D
 
-func _input(event):
+#func _input(event):
+#	if event is InputEventMouse and event.is_pressed():
+#		get_parent().load_area("res://test_bed.tscn")
+
+
+func _input_event(viewport, event, shape_idx):
 	if event is InputEventMouse and event.is_pressed():
 		get_parent().load_area("res://test_bed.tscn")
-
-
-#func _input_event(viewport, event, shape_idx):
-	#if event is InputEventMouse and event.is_pressed():
-		#print("test")
